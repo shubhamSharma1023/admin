f5ec4b33d6721bc71a180802cc15ce38ceb617c8
Shubham <shubhamSharma1023@gmail.com>
2019-12-11 18:55:08 +0530

    new reporting panel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Godseye</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">  
+            
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i&display=swap" rel="stylesheet">
+    <!-- data table css-->
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.css">
+    
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+ 
+    <!-- toggle css -->
+    <link href="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.6.1/css/bootstrap4-toggle.min.css" rel="stylesheet">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <section class="login_section">
+        <div class="login_left">
+        <!-- <img src="img/godseye.png" class="login_logo img-fluid"> -->
+        </div>
+        <div class="login_right">
+            <div class="login_card">
+                <div class="login_box">
+                    <p class="text-center g_wel">Welcome!</p>
+                    <div class="bg_login">
+                    <form>
+  <div class="form-group">
+    <input type="email" class="form-control g_input" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Login">
+  
+  </div>
+  <div class="form-group">
+    <input type="password" class="form-control g_input" id="exampleInputPassword1" placeholder="Password">
+  </div>
+
+  <button type="submit" class="btn g_btn sign_in">Sign In</button>
+   <div class="form-group form-check text-center">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" style="color: #000" for="exampleCheck1">Keep me logged in</label>
+  </div>
+</form>
+</div>
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
